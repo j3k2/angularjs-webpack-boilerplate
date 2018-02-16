357b89b55138d3c8a51cb83a5f537478fca868ac
John Kim <johnkim512@gmail.com>
2018-02-15 21:53:36 -0800

    fix typo

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -3,7 +3,7 @@ import config from '../config/config.module';
 export default {
   name: 'usersService',
   factory: ['$http', function ($http) {
-    const resourceBase = 'usesrs/';
+    const resourceBase = 'users/';
     return {
       getUsers() {
         return $http.get(config.api.base + resourceBase);
